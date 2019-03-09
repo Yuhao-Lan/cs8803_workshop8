eab4bf6925ae56252d285280ea7c0e31fd0c41a6
Nan Qin <qinnan0416@gmail.com>
2019-03-08 19:16:48 -0500

    create-vm

--- mapreduce/create_vm.py
+++ mapreduce/create_vm.py
@@ -4,7 +4,7 @@ number_node = 3
 node_IP_start = 10 # 10.0.0.10
 
 resource_group_name = "nqin8-resource-group"
-image_name = "nqin8-workshop-image"
+image_name = "nqin8-workshop8-image"
 admin_name = "nan"
 ssh_public_key_location = "~/.ssh/id_rsa.pub"
 vm_size = "Standard_B1s"
