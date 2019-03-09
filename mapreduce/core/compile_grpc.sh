716504b056c5249f49e10ae0ff8767bf959ab4f5
Nan Qin <qinnan0416@gmail.com>
2019-03-08 20:29:54 -0500

    compile grpc

--- mapreduce/core/compile_grpc.sh
+++ mapreduce/core/compile_grpc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir rpc_generated
-protoc hello.proto --cpp_out=rpc_generated
-protoc hello.proto --grpc_out=rpc_generated --plugin=protoc-gen-grpc=/usr/local/bin/grpc_cpp_plugin
+protoc master-worker.proto --cpp_out=rpc_generated
+protoc master-worker.proto --grpc_out=rpc_generated --plugin=protoc-gen-grpc=/usr/local/bin/grpc_cpp_plugin
