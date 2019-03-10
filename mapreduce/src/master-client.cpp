53123bfb740994cc7d5730e93a1f2d736e117f76
Nan Qin <qinnan0416@gmail.com>
2019-03-10 01:28:34 -0500

    all

--- mapreduce/src/master-client.cpp
+++ mapreduce/src/master-client.cpp
@@ -33,11 +33,11 @@ int main(int argc, char** argv) {
   LOG(INFO) << "Master splitted blob file: " << blob_filename << " into " << num_chunk << " chunk";
   // create M clients, where M is the number of worker nodes
   //create_client_handles(workers, &client_handles, &number_workers);
-  vector<WorkerStruct> vct;
-  create_client_handles(&vct);
-  WorkerStruct temp = vct[0];
-  LOG(INFO) << "StartMapper: " << blob_filename << ".1"; 
-  temp.handle->StartMapper(blob_filename + ".1");
+  //vector<WorkerStruct> vct;
+  //create_client_handles(&vct);
+  //WorkerStruct temp = vct[0];
+  //LOG(INFO) << "StartMapper: " << blob_filename << ".1"; 
+  //temp.handle->StartMapper(blob_filename + ".1");
   // start N pthreads, each thread selects a client based on round robin, and then calls cli.startmapper();
   /*for(int i = 0; i < num_chunk; i++){
 
