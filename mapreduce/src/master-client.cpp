f6e8ab51bde9bbb70b5263f4ffc152015e3aadb5
Nan Qin <qinnan0416@gmail.com>
2019-03-10 21:01:34 -0400

    all

--- mapreduce/src/master-client.cpp
+++ mapreduce/src/master-client.cpp
@@ -24,21 +24,21 @@ void start_mapper(string file_chunk){
   //
   while(1){
     int local_client_id = 0;
-    next_client_mtx.lock();
+    //next_client_mtx.lock();
     local_client_id = next_client;
     next_client ++;
     if(next_client >= vct.size()){
       next_client = 0;
     }
     worker_hostname = vct[local_client_id];
-    next_client_mtx.unlock();
+    //next_client_mtx.unlock();
     MasterClient cli(grpc::CreateChannel(worker_hostname + ":50051", grpc::InsecureChannelCredentials()));
-    // LOG(INFO) << "StartMapper: " << file_chunk << ". Using worker node: " << worker_hostname; 
+    LOG(INFO) << ".....StartMapper: " << file_chunk << ". Using worker node: " << worker_hostname; 
     output_file = cli.StartMapper(file_chunk);
     if(output_file != "RPC failed"){
       break;
     }else{
-      cout << "retry " << file_chunk << endl;
+      cout << "failed with worker: " << worker_hostname << ", retry " << file_chunk << endl;
       this_thread::sleep_for(chrono::seconds(2));
     }
   }
