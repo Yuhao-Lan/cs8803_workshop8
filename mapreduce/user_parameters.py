7347acc5b94aed6b24af162a66d0c061bc10d86e
Nan Qin <qinnan0416@gmail.com>
2019-03-24 16:16:50 -0400

    l

--- mapreduce/user_parameters.py
+++ mapreduce/user_parameters.py
@@ -3,6 +3,7 @@ number_node = 5
 number_master = 2
 node_IP_start = 10 # e.g. 10.0.0.10
 resource_group_name = "nqin8-resource-group"
+#resource_group_name = "map-reduce-group"
 image_name = "nqin8-workshop8-image"
 admin_name = "nan"
 ssh_public_key_location = "~/.ssh/id_rsa.pub"
